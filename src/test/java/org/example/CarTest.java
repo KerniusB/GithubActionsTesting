2bbb56b586686b57a51ac8d6b7d5a852e57020f8
Kernius Brazauskas <brazauskaskernius@gmail.com>
2021-12-07 18:03:09 +0200

    added test

--- src/test/java/org/example/CarTest.java
+++ src/test/java/org/example/CarTest.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class CarTest extends TestCase {
 
+    @Test
     public void testSetGetMaxSpeed() {
         Car car = new Car();
         car.setMaxSpeed(10);
