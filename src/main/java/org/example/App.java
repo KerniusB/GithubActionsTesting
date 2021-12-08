60e7f201545d90165f87234ae1ee772c55c838c8
Kernius Brazauskas <brazauskaskernius@gmail.com>
2021-12-08 13:30:19 +0200

    test1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,8 +8,5 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!" );
-        System.out.println( "Testing github actions" );
-        System.out.println( "Test 2" );
     }
 }
